0ef8e2598aced51f562c58315d198acaf2094d42
Yamamoto19034 <hj-g19034@sist.ac.jp>
2020-09-23 14:19:36 +0900

    雛形の作成

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 
 //########## ヘッダーファイル読み込み ##########
 #include "DxLib.h"
+#include "resource.h"
 
 //########## マクロ定義 ##########
 #define GAME_WIDTH	800	//画面の横の大きさ
@@ -12,33 +13,225 @@
 #define GAME_WINDOW_BAR	0	//タイトルバーはデフォルトにする
 #define GAME_WINDOW_NAME	"GAME TITLE"	//ウィンドウのタイトル
 
+#define GAME_FPS	60   //FPSの数値
+
+//マウスのボタン
+#define MOUSE_BUTTON_CODE  129 //0x0000～0x0080まで
+
+//キーボードの種類
+#define KEY_CODE_KIND	   256  //256種類
+
+//パスの長さ
+#define PATH_MAX		   255  //255文字まで
+#define NAME_MAX		   255  //255文字まで
+
+//フォント
+#define FONT_TANU_PATH	   TEXT(".\\FONT\\TanukiMagic.ttf")  //フォントの場所
+#define FONT_TANU_NAME     TEXT("たぬき油性マジック")		 //フォントの名前
+
+//エラーメッセージ
+#define FONT_INSTALL_ERR_TITLE  TEXT("フォントインストールエラー")
+#define FONT_CREATE_ERR_TITLE   TEXT("フォント作成エラー")
+
+//エラーメッセージ
+#define IMAGE_LOAD_ERR_TITLE    TEXT("画像読み込みエラー")
+
+//エラーメッセージ
+#define MUSIC_LOAD_ERR_TITLE    TEXT("音楽読み込みエラー")
+
+//閉じるボタンを押したとき
+#define MSG_CLOSE_TITLE			TEXT("終了メッセージ")
+#define MSG_CLOSE_CAPTION		TEXT("ゲームを終了しますか？")
+
+enum GAME_SCENE {
+	GAME_SCENE_START,
+	GAME_SCENE_PLAY,
+	GAME_SCENE_END,
+};  //ゲームのシーン
+
+//int型のPOINT構造体
+typedef struct STRUCT_I_POINT
+{
+	int x = -1;   //座標を初期化
+	int y = -1;   //座標を初期化
+}iPOINT;
+
+//マウス構造体
+typedef struct STRUCT_MOUSE
+{
+	int InputValue = 0;  //GetMouseInputの値を入れる
+	int WheelValue = 0;  //マウスホイールの回転量(奥はプラス値 / 手前はマイナス値)
+	iPOINT Point;        //マウスの座標が入る
+	iPOINT OldPoint;     //マウスの座標(直前)が入る
+	int OldButton[MOUSE_BUTTON_CODE] = { 0 };  //マウスのボタン入力(直前)が入る
+	int Button[MOUSE_BUTTON_CODE] = { 0 };     //マウスのボタン入力が入る
+}MOUSE;
+
+//フォント構造体
+typedef struct STRUCT_FONT
+{
+	char path[PATH_MAX];  //パス名
+	char name[NAME_MAX];  //フォント名
+	int handle;			  //ハンドル
+	int size;             //大きさ
+	int bold;			  //太さ
+	int type;			  //タイプ
+}FONT;
+
+typedef struct STRUCT_IMAGE
+{
+	char path[PATH_MAX];  //パス
+	int handle;			  //ハンドル
+	int x;				  //X位置
+	int y;				  //Y位置
+	int width;			  //幅
+	int height;			  //高さ
+	BOOL IsDraw = FALSE;  //描画できるか
+}IMAGE;  //画像構造体
+
+typedef struct STRUCT_MUSIC
+{
+	char path[PATH_MAX];  //パス
+	int handle;			  //ハンドル
+}MUSIC;  //音楽構造体
+
+typedef struct STRUCT_CHARA
+{
+	IMAGE image;		  //IMAGE構造体
+	int speed;			  //速さ
+
+	RECT coll;			  //当たり判定
+	iPOINT collBeforePt;  //当たる前の座標
+}CHARA;  //キャラクター構造体
+
+//########## グローバル変数 ##########
+//FPS関連
+int StartTimeFps;				//測定開始時刻
+int CountFps;					//カウンタ
+float CalcFps;					//計算結果
+int SampleNumFps = GAME_FPS;	//平均をとるサンプル数
+
+//キーボードの入力を取得
+char AllKeyState[KEY_CODE_KIND] = { '\0' };     //全てのキーの状態が入る
+char OldAllKeyState[KEY_CODE_KIND] = { '\0' };  //全てのキーの状態(直前)が入る
+
+//マウスの座標を取得
+MOUSE mouse;
+
+//ゲーム関連
+int GameScene;  //ゲームシーンを管理
+
+//プレイヤー関連
+CHARA player;   //ゲームのキャラ
+
+//########## プロトタイプ宣言 ##########
+VOID MY_FPS_UPDATE(VOID);		//FPS値を計測、更新する
+VOID MY_FPS_DRAW(VOID);			//FPS値を描画する
+VOID MY_FPS_WAIT(VOID);
+
+VOID MY_ALL_KEYDOWN_UPDATE(VOID);
+BOOL MY_KEY_DOWN(int);
+BOOL MY_KEY_UP(int);
+BOOL MY_KEYDOWN_KEEP(int, int);
+
+VOID MY_MOUSE_UPDATE(VOID);
+BOOL MY_MOUSE_DOWN(int);
+BOOL MY_MOUSE_UP(int);
+BOOL MY_MOUSEDOWN_KEEP(int, int);
+
+BOOL MY_FONT_INSTALL_ONCE(VOID);
+VOID MY_FONT_UNINSTALL_ONCE(VOID);
+BOOL MY_FONT_CREATE(VOID);
+VOID MY_FONT_DELETE(VOID);
+
+VOID MY_START(VOID);
+VOID MY_START_PROC(VOID);
+VOID MY_START_DRAW(VOID);
+
+VOID MY_PLAY_INIT(VOID);
+VOID MY_PLAY(VOID);
+VOID MY_PLAY_PROC(VOID);
+VOID MY_PLAY_DRAW(VOID);
+
+VOID MY_END(VOID);
+VOID MY_END_PROC(VOID);
+VOID MY_END_DRAW(VOID);
+
+BOOL MY_LOAD_IMAGE(VOID);
+VOID MY_DELETE_IMAGE(VOID);
+
+BOOL MY_LOAD_MUSIC(VOID);
+VOID MY_DELETE_MUSIC(VOID);
+
 //########## プログラムで最初に実行される関数 ##########
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
+	SetOutApplicationLogValidFlag(FALSE);		//Log.txtを出力しない
 	ChangeWindowMode(TRUE);						//ウィンドウモードに設定
 	SetGraphMode(GAME_WIDTH, GAME_HEIGHT, GAME_COLOR);	//指定の数値でウィンドウを表示する
 	SetWindowStyleMode(GAME_WINDOW_BAR);		//タイトルバーはデフォルトにする
 	SetMainWindowText(TEXT(GAME_WINDOW_NAME));	//ウィンドウのタイトルの文字
 	SetAlwaysRunFlag(TRUE);						//非アクティブでも実行する
+	SetWindowIconID(IDI_ICON1);					//アイコンファイルを読込
+	SetWindowUserCloseEnableFlag(FALSE);		//閉じるボタンで、勝手にウィンドウが閉じないようにする
 
 	if (DxLib_Init() == -1) { return -1; }	//ＤＸライブラリ初期化処理
 
-	int DrawX = 0;	//表示位置X
-	int DrawY = 0;	//表示位置Y
+	//画像を読み込む
+	if (MY_LOAD_IMAGE() == FALSE) { return -1; }
+
+	//音楽を読み込む
+	if (MY_LOAD_MUSIC() == FALSE) { return -1; }
+
+	//フォントを一時的にインストール
+	if (MY_FONT_INSTALL_ONCE() == FALSE) { return -1; }
+
+	//フォントハンドルを作成
+	if (MY_FONT_CREATE() == FALSE) { return -1; }
+
+	GameScene = GAME_SCENE_START;   //ゲームシーンはスタート画面から
+	SetDrawScreen(DX_SCREEN_BACK);  //Draw系関数は裏画面に描画
 
 	//▼▼▼▼▼ プログラム追加ここから ▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼
-	SetDrawScreen(DX_SCREEN_BACK);	//Draw系関数は裏画面に描画
 
 	//無限ループ
 	while (TRUE)
 	{
 		if (ProcessMessage() != 0) { break; }	//メッセージ処理の結果がエラーのとき、強制終了
-
 		if (ClearDrawScreen() != 0) { break; }	//画面を消去できなかったとき、強制終了
 
-		DrawString(DrawX, DrawY, "Hello World", GetColor(255, 255, 255));	//文字を描画
+		//画面の閉じるボタンが押されたとき
+		if (GetWindowUserCloseFlag(TRUE))
+		{
+			int Ret = MessageBox(GetMainWindowHandle(), MSG_CLOSE_CAPTION, MSG_CLOSE_TITLE, MB_YESNO | MB_ICONASTERISK);
+			if (Ret == IDYES) { break; }  //YESなら、ゲームを中断する
+		}
+
+		MY_ALL_KEYDOWN_UPDATE();  //押しているキー状態を取得する
+
+		MY_MOUSE_UPDATE();
+
+		MY_FPS_UPDATE();
+
+		//シーンごとに処理を行う
+		switch (GameScene)
+		{
+		case GAME_SCENE_START:
+			MY_START();   //スタート画面
+			break;
+		case GAME_SCENE_PLAY:
+			MY_PLAY();    //プレイ画面
+			break;
+		case GAME_SCENE_END:
+			MY_END();     //エンド画面
+			break;
+		}
+
+		MY_FPS_DRAW();
 
 		ScreenFlip();		//モニタのリフレッシュレートの速さで裏画面を再描画
+
+		MY_FPS_WAIT();
 	}
 	//▲▲▲▲▲ プログラム追加ここまで ▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲
 
@@ -47,7 +240,393 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	//WaitKey();	//キー入力待ち
 	//△△△△△ プログラム削除ここまで △△△△△△△△△△△△△△△△△△△△
 
+	//フォントハンドルを破棄
+	MY_FONT_DELETE();
+
+	//一時的にインストールしたフォントをアンインストール
+	MY_FONT_UNINSTALL_ONCE();
+
+	//画像ハンドルを破棄
+	MY_DELETE_IMAGE();
+
+	//音楽ハンドルを破棄
+	MY_DELETE_MUSIC();
+
 	DxLib_End();	//ＤＸライブラリ使用の終了処理
 
 	return 0;
+}
+
+//########## FPS値を計測、更新する関数 ##########
+VOID MY_FPS_UPDATE(VOID)
+{
+	if (CountFps == 0) //1フレーム目なら時刻を記憶
+	{
+		StartTimeFps = GetNowCount();
+	}
+
+	if (CountFps == SampleNumFps)  //60フレーム目なら平均を計算
+	{
+		int now = GetNowCount();
+		//現在の時間から、0フレーム目の時間を引き、FPSの数値で割る＝1FPS辺りの平均時間が計算される
+		CalcFps = 1000.f / ((now - StartTimeFps) / (float)SampleNumFps);
+		CountFps = 0;
+		StartTimeFps = now;
+	}
+	CountFps++;
+	return;
+}
+
+//########## FPS値を描画する関数 ##########
+VOID MY_FPS_DRAW(VOID)
+{
+	//文字列を描画
+	DrawFormatString(0, GAME_HEIGHT - 20, GetColor(255, 255, 255), "FPS:%.1f", CalcFps);
+	return;
+}
+
+//########## FPS値を計測し、待つ関数 ##########
+VOID MY_FPS_WAIT(VOID)
+{
+	int resultTime = GetNowCount() - StartTimeFps;
+	int waitTime = CountFps * 1000 / GAME_FPS - resultTime;
+
+	if (waitTime > 0)
+	{
+		WaitTimer(waitTime);
+	}
+	return;
+}
+
+//########## キーの入力状態を更新する関数 ##########
+VOID MY_ALL_KEYDOWN_UPDATE(VOID)
+{
+	//一時的に、現在のキーの入力状態を格納する
+	char TempKey[KEY_CODE_KIND];
+
+	//直前のキー入力をとっておく
+	for (int i = 0; i < KEY_CODE_KIND; i++)
+	{
+		OldAllKeyState[i] = AllKeyState[i];
+	}
+
+	GetHitKeyStateAll(TempKey); // 全てのキーの入力状態を得る
+
+	for (int i = 0; i < KEY_CODE_KIND; i++)
+	{
+		if (TempKey[i] != 0)	//押されているキーのキーコードを押しているとき
+		{
+			AllKeyState[i]++;	//押されている
+		}
+		else
+		{
+			AllKeyState[i] = 0;	//押されていない
+		}
+	}
+	return;
+}
+
+//キーを押しているか、キーコードで判断する
+//引　数：int：キーコード：KEY_INPUT_???
+BOOL MY_KEY_DOWN(int KEY_INPUT_)
+{
+	//キーコードのキーを押している時
+	if (AllKeyState[KEY_INPUT_] != 0)
+	{
+		return TRUE;	//キーを押している
+	}
+	else
+	{
+		return FALSE;	//キーを押していない
+	}
+}
+
+//キーを押し上げたか、キーコードで判断する
+//引　数：int：キーコード：KEY_INPUT_???
+BOOL MY_KEY_UP(int KEY_INPUT_)
+{
+	if (OldAllKeyState[KEY_INPUT_] >= 1	//直前は押していて
+		&& AllKeyState[KEY_INPUT_] == 0)	//今は押していないとき
+	{
+		return TRUE;	//キーを押し上げている
+	}
+	else
+	{
+		return FALSE;	//キーを押し上げていない
+	}
+}
+
+
+//キーを押し続けているか、キーコードで判断する
+//引　数：int：キーコード：KEY_INPUT_???
+//引　数：int：キーを押し続ける時間
+BOOL MY_KEYDOWN_KEEP(int KEY_INPUT_, int DownTime)
+{
+	//押し続ける時間=秒数×FPS値
+	//例）60FPSのゲームで、1秒間押し続けるなら、1秒×60FPS
+	int UpdateTime = DownTime * GAME_FPS;
+
+	if (AllKeyState[KEY_INPUT_] > UpdateTime)
+	{
+		return TRUE;	//押し続けている
+	}
+	else
+	{
+		return FALSE;	//押し続けていない
+	}
+}
+
+//マウスの入力情報を更新する
+VOID MY_MOUSE_UPDATE(VOID)
+{
+	//マウスの以前の座標を取得
+	mouse.OldPoint = mouse.Point;
+
+	//マウスの以前のボタン入力を取得
+	for (int i = 0; i < MOUSE_BUTTON_CODE; i++) { mouse.OldButton[i] = mouse.Button[i]; }
+
+	//マウスの座標を取得
+	GetMousePoint(&mouse.Point.x, &mouse.Point.y);
+
+	//マウスの押しているボタンを取得
+	mouse.InputValue = GetMouseInput();
+
+	//左ボタンを押しているかチェック(TRUEは0以外なので、2も4もTRUE)
+	if ((mouse.InputValue & MOUSE_INPUT_LEFT) == MOUSE_INPUT_LEFT) { mouse.Button[MOUSE_INPUT_LEFT]++; }		//押している
+	if ((mouse.InputValue & MOUSE_INPUT_LEFT) != MOUSE_INPUT_LEFT) { mouse.Button[MOUSE_INPUT_LEFT] = 0; }		//押していない
+
+	//中ボタンを押しているかチェック(TRUEは0以外なので、2も4もTRUE)
+	if ((mouse.InputValue & MOUSE_INPUT_MIDDLE) == MOUSE_INPUT_MIDDLE) { mouse.Button[MOUSE_INPUT_MIDDLE]++; }	//押している
+	if ((mouse.InputValue & MOUSE_INPUT_MIDDLE) != MOUSE_INPUT_MIDDLE) { mouse.Button[MOUSE_INPUT_MIDDLE] = 0; }	//押していない
+
+	//右ボタンを押しているかチェック(TRUEは0以外なので、2も4もTRUE)
+	if ((mouse.InputValue & MOUSE_INPUT_RIGHT) == MOUSE_INPUT_RIGHT) { mouse.Button[MOUSE_INPUT_RIGHT]++; }		//押している
+	if ((mouse.InputValue & MOUSE_INPUT_RIGHT) != MOUSE_INPUT_RIGHT) { mouse.Button[MOUSE_INPUT_RIGHT] = 0; }	//押していない
+
+	//ホイールの回転量を取得
+	mouse.WheelValue = GetMouseWheelRotVol();
+
+	return;
+}
+
+//ボタンを押しているか、マウスーコードで判断する
+//引　数：int：マウスコード：MOUSE_INPUT_???
+BOOL MY_MOUSE_DOWN(int MOUSE_INPUT_)
+{
+	//マウスコードのボタンを押している時
+	if (mouse.Button[MOUSE_INPUT_] != 0)
+	{
+		return TRUE;	//ボタンを押している
+	}
+	else
+	{
+		return FALSE;	//ボタンを押していない
+	}
+}
+
+//ボタンを押し上げたか、マウスコード判断する
+//引　数：int：マウスコード：MOUSE_INPUT_???
+BOOL MY_MOUSE_UP(int MOUSE_INPUT_)
+{
+	if (mouse.OldButton[MOUSE_INPUT_] >= 1	//直前は押していて
+		&& mouse.Button[MOUSE_INPUT_] == 0)	//今は押していないとき
+	{
+		return TRUE;	//ボタンを押し上げている
+	}
+	else
+	{
+		return FALSE;	//ボタンを押し上げていない
+	}
+}
+
+//ボタンを押し続けているか、マウスコード判断する
+//引　数：int：マウスコード：MOUSE_INPUT_???
+//引　数：int：ボタンを押し続ける時間
+BOOL MY_MOUSEDOWN_KEEP(int MOUSE_INPUT_, int DownTime)
+{
+	//押し続ける時間=秒数×FPS値
+	//例）60FPSのゲームで、1秒間押し続けるなら、1秒×60FPS
+	int UpdateTime = DownTime * GAME_FPS;
+
+	if (mouse.Button[MOUSE_INPUT_] > UpdateTime)
+	{
+		return TRUE;	//押し続けている
+	}
+	else
+	{
+		return FALSE;	//押し続けていない
+	}
+}
+
+//フォントをこのソフト用に、一時的にインストール
+BOOL MY_FONT_INSTALL_ONCE(VOID)
+{
+	//フォントを一時的に読み込み(WinAPI)
+	if (AddFontResourceEx(FONT_TANU_PATH, FR_PRIVATE, NULL) == 0)
+	{
+		//エラーメッセージ表示
+		MessageBox(GetMainWindowHandle(), FONT_TANU_NAME, FONT_INSTALL_ERR_TITLE, MB_OK);
+		return FALSE;
+	}
+
+	return TRUE;
+}
+
+//フォントをこのソフト用に、一時的にアンインストール
+VOID MY_FONT_UNINSTALL_ONCE(VOID)
+{
+	//一時的に読み込んだフォントを削除(WinAPI)
+	RemoveFontResourceEx(FONT_TANU_PATH, FR_PRIVATE, NULL);
+
+	return;
+}
+
+//フォントを読み込む関数
+//引　数：const char *：読み込むフォントの名前
+//引　数：int　：フォントの大きさ
+//引　数：int　：フォントの太さ
+//引　数：int　：フォントのタイプ
+//戻り値：BOOL ：エラー時はFALSE / 正常時はTRUE
+BOOL MY_FONT_CREATE(VOID)
+{
+	return TRUE;
+}
+
+//フォントを削除する関数
+VOID MY_FONT_DELETE(VOID)
+{
+
+	return;
+}
+
+//スタート画面
+VOID MY_START(VOID)
+{
+	MY_START_PROC();	//スタート画面の処理
+	MY_START_DRAW();	//スタート画面の描画
+
+
+	DrawString(0, 0, "スタート画面(エンターキーを押して下さい)", GetColor(255, 255, 255));
+	return;
+}
+
+//スタート画面の処理
+VOID MY_START_PROC(VOID)
+{
+	//エンターキーを押したら、プレイシーンへ移動する
+	if (MY_KEY_DOWN(KEY_INPUT_RETURN) == TRUE)
+	{
+		MY_PLAY_INIT();	//ゲーム初期化
+
+		//ゲームのシーンをプレイ画面にする
+		GameScene = GAME_SCENE_PLAY;
+
+		return;
+	}
+
+	return;
+}
+
+//スタート画面の描画
+VOID MY_START_DRAW(VOID)
+{
+
+	return;
+}
+
+//プレイ画面初期化
+VOID MY_PLAY_INIT(VOID)
+{
+
+	return;
+}
+
+//プレイ画面
+VOID MY_PLAY(VOID)
+{
+	MY_PLAY_PROC();	//プレイ画面の処理
+	MY_PLAY_DRAW();	//プレイ画面の描画
+
+	DrawString(0, 0, "プレイ画面(スペースキーを押して下さい)", GetColor(255, 255, 255));
+	return;
+}
+
+//プレイ画面の処理
+VOID MY_PLAY_PROC(VOID)
+{
+	//スペースキーを押したら、エンドシーンへ移動する
+	if (MY_KEY_DOWN(KEY_INPUT_SPACE) == TRUE)
+	{
+		//ゲームのシーンをエンド画面にする
+		GameScene = GAME_SCENE_END;
+
+		return;
+	}
+
+	return;
+}
+
+//プレイ画面の描画
+VOID MY_PLAY_DRAW(VOID)
+{
+
+	return;
+}
+
+//エンド画面
+VOID MY_END(VOID)
+{
+	MY_END_PROC();	//エンド画面の処理
+	MY_END_DRAW();	//エンド画面の描画
+
+
+	return;
+}
+
+//エンド画面の処理
+VOID MY_END_PROC(VOID)
+{
+	//エスケープキーを押したら、スタートシーンへ移動する
+	if (MY_KEY_DOWN(KEY_INPUT_ESCAPE) == TRUE)
+	{
+		GameScene = GAME_SCENE_START;
+	}
+
+	DrawString(0, 0, "エンド画面(エスケープキーを押して下さい)", GetColor(255, 255, 255));
+	return;
+}
+
+//エンド画面の描画
+VOID MY_END_DRAW(VOID)
+{
+
+	return;
+}
+
+//画像をまとめて読み込む関数
+BOOL MY_LOAD_IMAGE(VOID)
+{
+
+	return TRUE;
+}
+
+//画像をまとめて削除する関数
+VOID MY_DELETE_IMAGE(VOID)
+{
+
+	return;
+}
+
+//音楽をまとめて読み込む関数
+BOOL MY_LOAD_MUSIC(VOID)
+{
+
+	return TRUE;
+}
+
+//音楽をまとめて削除する関数
+VOID MY_DELETE_MUSIC(VOID)
+{
+
+	return;
 }
